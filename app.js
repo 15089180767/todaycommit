696df0f6b127b310a7cb6de01b2c8afee5b2d48a
袁杰 <1058426392@qq.com>
2023-02-28 16:04:54 +0800

    添加注释

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const { exec } = require('child_process');
 const { writeFile } = require('fs');
-const instruc = `git log --oneline --since=yesterday`;
-const cwd = `E:/fengye/Project/Kalos`;
-const outPutPath = `C:/Users/15640/Desktop/todayCommit/`;
+
+
+const cwd = `E:/fengye/Project/Kalos`; // 项目路径
+const outPutPath = `C:/Users/15640/Desktop/todayCommit/`; // 保存路径
+
+
 const sd = require('silly-datetime');
 const today = sd.format(new Date(), 'YYYY-MM-DD');
 exec(`git -C ${cwd} log --oneline --after="${today} 00:00" --before="${today} 23:59"`, (err, stdout, stderr) => {
